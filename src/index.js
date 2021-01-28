591b8291cb5c4d875233e69a453c45b8519d8a48
Dmytro Orgish <isnl.dd@gmail.com>
2021-01-28 19:31:33 +0200

    finish hw10

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+import menuTemplate from './templates/menu.hbs';
+import menu from './menu.json';
+
+const refMenu = document.querySelector('.js-menu');
+const markup = menuTemplate(menu);
+
+refMenu.insertAdjacentHTML('beforeend', markup);
+console.log('hello');
